3cb8472d668f1aaacfebb0f91fd41889b8824e2a
Asad Faraz <asadfaraz44@gmail.com>
2021-04-20 12:57:14 +0500

    products link added to home

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ This is the basic setup and how Express js work. Express is a third party module
 performing other server stuff.
 
 I am learning Nodejs and here I learned how to make get request using Expressjs.
+
+\***\*\*\*\*** Methods to use with Express.js **\*\*\***
+1- GET
+2- POST
+3- PUT
+4- DELETE
+5- ALL
+6- USE
+7- LISTEN
+
+---
+
+I also have a data.js file where I have some dummy data in json. The data.js have 2 arrays one for
+products and one for people. We will use these while working with route params and query strings etc.
--- learning-express.js
+++ learning-express.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const { products } = require("./data");
 
 const app = express();
 
 // HOme Page
 app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
+  // res.status(200).send("Home Page");
   console.log(req.path);
+
+  // link to product page
+  res.send("<h3> Home Page </h3> <a href='/api/products'>Products</a>");
+});
+// products page
+app.get("/api/products", (req, res) => {
+  // res.send(products);
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+
+    return { id, name, image };
+  });
+  res.send(newProducts);
 });
 
 // About page
@@ -23,15 +37,3 @@ app.all("*", (req, res) => {
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
 });
-
-/* Methods to use with Express.js
-
-1- GET
-2- POST
-3- PUT
-4- DELETE
-5- ALL
-6- USE
-7- LISTEN
-
-*/
