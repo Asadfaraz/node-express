a8d9061210dc6bd2085c0e27b5b644f0bf0704ae
Asad Faraz <asadfaraz44@gmail.com>
2021-04-20 23:56:55 +0500

    middleware app.use

---
+++ logger.js
@@ -0,0 +1,11 @@
+// Simple Middleware --> it is just a function
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+
+  next();
+};
+
+module.exports = logger;
--- middles.js
+++ middles.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const app = express();
+const logger = require("./logger");
 
-// Simple Middleware --> it is just a function
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
+// Middleware using app.use
+app.use(logger);
 
-  next();
-};
-
+// ----------------------------------------
 // HOme route
 app.get("/", logger, (req, res) => {
   res.send("Home");
@@ -21,6 +16,16 @@ app.get("/about", logger, (req, res) => {
   res.send("About");
 });
 
+// other routes
+app.get("/api/products", (req, res) => {
+  res.send("products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("items");
+});
+
+// ------------------------------------------
 app.listen(5000, () => {
   console.log("app is listening on port 5000");
 });
